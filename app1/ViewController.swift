b2fd928e74f281d3253388543a86bf66de92c11e
Nagapranaya Vemula <nagapranaya.vemula@gmail.com>
2023-10-30 15:53:42 +0530

    Initial Commit

---
+++ app1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app1
+//
+//  Created by Pranaya on 10/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
